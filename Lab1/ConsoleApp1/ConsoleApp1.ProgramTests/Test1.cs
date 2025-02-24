db847d58f60a925bd7678ee9b36bd85a6ef8df83
IhorKrav <kravetsiw@gmail.com>
2025-02-24 10:56:42 +0200

    Lab1 commit

---
+++ Lab1/ConsoleApp1/ConsoleApp1.ProgramTests/Test1.cs
@@ -0,0 +1,176 @@
+﻿namespace ConsoleApp1.ProgramTests;
+
+using ConsoleApp1.Program;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+  [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void TestAddNode()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+
+            // Act
+            graph.AddNode(1);
+
+            // Assert
+            Assert.IsTrue(graph.AdjacencyList.ContainsKey(1));
+            Assert.AreEqual(0, graph.AdjacencyList[1].Count);
+        }
+
+        [TestMethod]
+        public void TestAddEdge()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+
+            // Act
+            graph.AddEdge(1, 2);
+
+            // Assert
+            Assert.IsTrue(graph.AdjacencyList.ContainsKey(1));
+            Assert.IsTrue(graph.AdjacencyList.ContainsKey(2));
+            Assert.IsTrue(graph.AdjacencyList[1].Contains(2));
+            Assert.IsTrue(graph.AdjacencyList[2].Contains(1)); // Since it's undirected
+        }
+
+        [TestMethod]
+        public void TestGraphInitializationEmpty()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+
+            // Assert
+            Assert.AreEqual(0, graph.AdjacencyList.Count);
+        }
+
+        [TestMethod]
+        public void TestAddMultipleNodes()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+
+            // Act
+            graph.AddNode(1);
+            graph.AddNode(2);
+            graph.AddNode(3);
+
+            // Assert
+            Assert.AreEqual(3, graph.AdjacencyList.Count);
+        }
+
+        [TestMethod]
+        public void TestAddingDuplicateNodes()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+
+            // Act
+            graph.AddNode(1);
+            graph.AddNode(1);  // Add duplicate
+
+            // Assert
+            Assert.AreEqual(1, graph.AdjacencyList.Count);
+        }
+
+        [TestMethod]
+        public void TestBFS()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(3, 5);
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                graph.BFS(1);
+
+                // Assert - BFS should visit nodes in level order
+                var output = sw.ToString().Trim();
+                Assert.IsTrue(output.Contains("1 2 3 4 5"));
+            }
+        }
+
+        [TestMethod]
+        public void TestDFS()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(3, 5);
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                graph.DFS(1);
+
+                // Assert - DFS should visit nodes in depth order
+                var output = sw.ToString().Trim();
+                Assert.IsTrue(output.Contains("1 2 4 3 5") || output.Contains("1 3 5 2 4"));
+            }
+        }
+
+        [TestMethod]
+        public void TestBFSWithNonExistentNode()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+            graph.AddEdge(1, 2);
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                graph.BFS(99);  // Node 99 does not exist
+
+                // Assert - No output expected
+                var output = sw.ToString().Trim();
+                Assert.AreEqual("Start node not found in the graph.", output);
+            }
+        }
+
+        [TestMethod]
+        public void TestDFSWithNonExistentNode()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+            graph.AddEdge(1, 2);
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                graph.DFS(99);  // Node 99 does not exist
+
+                // Assert - No output expected
+                var output = sw.ToString().Trim();
+                Assert.AreEqual("Start node not found in the graph.", output);
+            }
+        }
+
+        [TestMethod]
+        public void TestGraphHasCorrectEdges()
+        {
+            // Arrange
+            var graph = new ProgramCode.Graph();
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+
+            // Assert
+            Assert.IsTrue(graph.AdjacencyList[1].Contains(2));
+            Assert.IsTrue(graph.AdjacencyList[2].Contains(1));
+            Assert.IsTrue(graph.AdjacencyList[2].Contains(3));
+            Assert.IsTrue(graph.AdjacencyList[3].Contains(2));
+            Assert.IsTrue(graph.AdjacencyList[3].Contains(4));
+            Assert.IsTrue(graph.AdjacencyList[4].Contains(3));
+        }
+    }
---
+++ Lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,140 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp1.Program
+{
+    public class ProgramCode
+    {
+       public class Graph
+        {
+        private Dictionary<int, List<int>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<int, List<int>>();
+        }
+
+        // Add a node to the graph
+        public void AddNode(int node)
+        {
+            if (!adjacencyList.ContainsKey(node))
+            {
+                adjacencyList[node] = new List<int>();
+            }
+        }
+
+        public Dictionary<int, List<int>> AdjacencyList
+        {
+            get { return adjacencyList; }
+        }
+
+        // Add an edge (connection) between two nodes
+        public void AddEdge(int node1, int node2)
+        {
+            if (!adjacencyList.ContainsKey(node1)) AddNode(node1);
+            if (!adjacencyList.ContainsKey(node2)) AddNode(node2);
+
+            adjacencyList[node1].Add(node2);
+            adjacencyList[node2].Add(node1); // Remove this for a directed graph
+        }
+
+        // Display the graph as an adjacency list
+        public void PrintGraph()
+        {
+            foreach (var node in adjacencyList)
+            {
+                Console.Write(node.Key + " -> ");
+                Console.WriteLine(string.Join(", ", node.Value));
+            }
+        }
+
+        // Breadth-First Search (BFS)
+        public void BFS(int startNode)
+        {
+           if (!adjacencyList.ContainsKey(startNode))
+                {
+                    Console.WriteLine("Start node not found in the graph.");
+                    return;
+                }
+
+                HashSet<int> visited = new HashSet<int>();
+                Queue<int> queue = new Queue<int>();
+
+                visited.Add(startNode);
+                queue.Enqueue(startNode);
+
+                Console.WriteLine("\nBFS Traversal:");
+                while (queue.Count > 0)
+                {
+                    int current = queue.Dequeue();
+                    Console.Write(current + " ");
+
+                    foreach (int neighbor in adjacencyList[current])
+                    {
+                        if (!visited.Contains(neighbor))
+                        {
+                            visited.Add(neighbor);
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+                Console.WriteLine();
+        }
+
+        // Depth-First Search (DFS)
+        public void DFS(int startNode)
+        {
+            if (!adjacencyList.ContainsKey(startNode))
+                {
+                    Console.WriteLine("Start node not found in the graph.");
+                    return;
+                }
+
+                HashSet<int> visited = new HashSet<int>();
+                Console.WriteLine("\nDFS Traversal:");
+                DFSHelper(startNode, visited);
+                Console.WriteLine();
+        }
+
+        private void DFSHelper(int node, HashSet<int> visited)
+        {
+            visited.Add(node);
+            Console.Write(node + " ");
+
+            foreach (int neighbor in adjacencyList[node])
+            {
+                if (!visited.Contains(neighbor))
+                {
+                    DFSHelper(neighbor, visited);
+                }
+            }
+        }
+        // static void Main()
+        // {
+        //     Graph graph = new Graph();
+
+        //     // Adding nodes and edges
+        //     graph.AddEdge(1, 2);
+        //     graph.AddEdge(1, 3);
+        //     graph.AddEdge(2, 4);
+        //     graph.AddEdge(2, 5);
+        //     graph.AddEdge(3, 6);
+        //     graph.AddEdge(3, 7);
+
+        //     // Display the graph
+        //     Console.WriteLine("Graph Adjacency List:");
+        //     graph.PrintGraph();
+
+        //     // Perform BFS & DFS
+        //     graph.BFS(1);
+        //     graph.DFS(1);
+        // }
+    }
+
+    
+      
+}
+}
+
+// Driver Code
+
+  
